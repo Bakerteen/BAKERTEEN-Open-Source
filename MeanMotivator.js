41706c80166c2fc22ff2c4b5a412c04d9407fbff
Bakerteen <43405165+Bakerteen@users.noreply.github.com>
2018-09-19 12:07:24 +0200

    Delete MeanMotivator.js

--- MeanMotivator.js
+++
@@ -1,9 +0,0 @@
-//TheMeanWeekendMotivator
-let currentTime = new Date()
-let day = currentTime.getDate()
-
-if (day === 'Saturday' || day === 'Sunday') {
-  console.log('Enjoy the weekend!');
-} else {
-  console.log('Do some work you lazy pickle');
-}
