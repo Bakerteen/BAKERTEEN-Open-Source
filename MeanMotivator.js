b8cb50fbe145691ec09b83e91dc0b151625adfdb
Bakerteen <43405165+Bakerteen@users.noreply.github.com>
2018-09-19 12:15:40 +0200

    MeanMotivator.js
    Motivate yourself by this awesome fun tool.

---
+++ MeanMotivator.js
@@ -0,0 +1,10 @@
+//TheMeanMotivator
+let currentTime = new Date()
+let day = currentTime.getDay();
+
+if (day === 6 || day === 7) {
+  console.log('Enjoy the weekend!');
+} else {
+  console.log('Do some work you lazy pickle');
+ 
+}
